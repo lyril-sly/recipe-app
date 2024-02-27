b3b8b40c7679d5e9260b6052d4c3fa4ea19e8fac
lyril-sly <sylvialyril.sl@gmail.com>
2024-02-27 12:35:55 +0000

    did the search

--- src/Pages/recipes/index.jsx
+++ src/Pages/recipes/index.jsx
@@ -1,27 +1,31 @@
 
 import { Card, CardMedia, Container, TextField, Grid, CardContent, Typography } from "@mui/material"
 import RecipeItems from "../../component/navbar/recipe-items"
-import {useState, useEffect} from "react"
+import { useState, useEffect } from "react"
 
 
 
 export default function Recipes() {
- const [recipes, setRecipes] = useState([]);
+    const [recipes, setRecipes] = useState([]);
+    const [searchItem, setSearchItem] = useState("")
 
- const searchRecipes = () => {
-    const url = new URL('https://api.spoonacular.com/recipes/complexSearch');
-    url.searchParams.append('apiKey', '695d1ce54a0a4588b595426645cd7ca0');
-    fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-        setRecipes(data.results);
-    })
-    .catch((error) => {
-        console.log(error)
-    })
- }
+    const searchRecipes = () => {
+        // the url is use to get the item or the images or whatever you want to get
+        const url = new URL('https://api.spoonacular.com/recipes/complexSearch');
+        url.searchParams.append('apiKey', process.env.REACT_APP_SPOONACULAR_API_KEY);
+        url.searchParams.append('query', searchItem);
 
- useEffect(searchRecipes, []);
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+                setRecipes(data.results);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+    // 
+    useEffect(searchRecipes, []);
     return (
 
         <>
@@ -31,15 +35,21 @@ export default function Recipes() {
                     fullWidth
                     id="outlined-basic"
                     label="Enter a key to display your recipe"
-                    variant="outlined" />
+                    variant="outlined"
+                    value={searchItem}
+                    // onChange means the person is typing something
+                     onChange={(event) => setSearchItem(event.target.value)} 
+                    onKeyDown={event => event.key === 'Enter' && searchRecipes()}
+                />
 
-
-                    <Grid sx={{mt: "2rem"}} container spacing={3}>
-
-                    {recipes.map((recipe) => <RecipeItems key={recipe.id} title={recipe.title} image={recipe.image}/>
+                <Grid sx={{ mt: "2rem" }} container spacing={3}>
+                    {/* the.map is use to render a list of iterms from an array */}
+                    {recipes.map((recipe) =>
+                        // so you pass the key on the list that you want to get from the api or the url then you pass each into the param at the child component
+                        <RecipeItems key={recipe.id} title={recipe.title} image={recipe.image} />
                     )}
 
-                    </Grid>
+                </Grid>
             </Container>
         </>
     )
